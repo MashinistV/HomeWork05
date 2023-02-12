d8a7d396677eee6ef5e7ad6bc3ae8951496be815
Sergey <mashinistv@gmail.com>
2023-02-12 14:54:05 +0300

    Complited task 36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 36. Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+string ofer = "Введите число элементов массива: ";
+int quantity = CheckNumber(ofer);
+int[] randomArray = RandomArray(quantity, -9, 9);
+Console.WriteLine($"Введён массив {string.Join(" ", randomArray)}");
+int sum = SumOddFromArray(randomArray);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях в массиве: {sum}");
+
+int SumOddFromArray(int[] array)
+{
+    int sum = 0;
+
+    for (int index = 0; index < array.Length; index = index + 2)
+    {
+        sum = sum + array[index];
+    }
+
+    return sum;
+}
+
+int[] RandomArray(int length, int minvalue, int maxvalue)
+{
+    int[] array = new int[length];
+    Random randomarray = new Random();
+
+    for (int count = 0; count < length; count++)
+    {
+        array[count] = randomarray.Next(minvalue, maxvalue + 1);
+    }
+
+    return array;
+}
+
+int CheckNumber(string text)
+{
+    Console.Write(text);
+    int number;
+
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("Вы ввели не число");
+        Console.Write("Введите число: ");
+    }
+
+    if (number <0) 
+    {
+        number = number * -1;
+    }
+
+    return number;
+}
